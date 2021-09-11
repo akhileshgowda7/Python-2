296aa1166cc86b70d932fe7fda60a55a550b7123
Akhilesh Gowda <50296948+akhileshgowda7@users.noreply.github.com>
2021-09-11 13:05:36 -0400

    Escape character
    Understanding how escape character works in python with several examples

---
+++ escapeChar.py
@@ -0,0 +1,13 @@
+splitString = "This string has been\nsplit\nseveral\nlines"
+print(splitString)
+tabbedString=("1\t2\t3\t4\t")
+print(tabbedString)
+print('The pet shop owner said "No, no,\'e\'s uh,...he\'s resting".')
+#OR
+print("The pet shop owner said \"No, no,'e's uh,...he's resting\".")
+#OR
+print("""The pet shop owner said "No, no,'e's uh,...he's resting".""")
+print("""This string has been
+split
+several
+lines""")
